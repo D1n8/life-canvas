11f20d1169f62545750203e3816bbf575ea9bb15
d1n8 <mr.dinyar@mail.ru>
2025-12-12 21:16:24 +0500

    add font and header

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="header-container">
+                Life<span className="blue">Canvas</span>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
